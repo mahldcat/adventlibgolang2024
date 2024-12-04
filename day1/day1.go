39ce562851b27bb407060712923583c6d8b41636
mahldcat <mahldcat@gmail.com>
2024-12-03 18:58:28 -0700

    adding day 1 solutions with unit tests

--- day1/day1.go
+++ day1/day1.go
@@ -2,10 +2,52 @@ package day1
 
 import (
 	"errors"
-	"fmt"
+	"sort"
 )
 
-func SolveDay1Part1() (int, error) {
-	fmt.Println("Entering Part 1")
-	return -1, errors.New("not implemented yet")
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func toFreq(slice []int) map[int]int {
+	freqMap := make(map[int]int)
+
+	// Calculate frequencies
+	for _, num := range slice {
+		freqMap[num]++
+	}
+
+	return freqMap
+}
+
+func SolveDay1Part1(left []int, right []int) (int, error) {
+	if len(left) != len(right) {
+		return -1, errors.New("left and right slices must be the same length")
+	}
+	sort.Ints(left)
+	sort.Ints(right)
+
+	fullDist := 0
+	for i := 0; i < len(left); i++ {
+		fullDist += abs(left[i] - right[i])
+	}
+
+	return fullDist, nil
+}
+
+func SolveDay1Part2(left []int, right []int) (int, error) {
+
+	fMap := toFreq(right)
+
+	distFreqCalc := 0
+
+	for _, num := range left {
+
+		distFreqCalc += (num * fMap[num])
+	}
+
+	return distFreqCalc, nil
 }
