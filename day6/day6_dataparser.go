cc59dea9ba977d1eac9c97cccf7fd62883042c31
mahldcat <mahldcat@gmail.com>
2024-12-06 15:08:15 -0700

    Day 6 part 2 is working and passing uTests

--- day6/day6_dataparser.go
+++ day6/day6_dataparser.go
@@ -5,12 +5,6 @@ import (
 	"strings"
 )
 
-type Vector struct {
-	X      int
-	Y      int
-	Facing rune
-}
-
 func day6DataParser(rawData string) ([][]rune, Vector, error) {
 	reader := strings.NewReader(rawData)
 	scanner := bufio.NewScanner(reader)
@@ -23,15 +17,14 @@ func day6DataParser(rawData string) ([][]rune, Vector, error) {
 		if line == "" { // Skip empty lines (optional, based on your needs)
 			continue
 		}
-		guardIdx := strings.IndexRune(line, '^')
 
-		if guardIdx == -1 {
-			grid = append(grid, []rune(line))
-		} else {
+		guardIdx := strings.IndexRune(line, '^')
+		if guardIdx != -1 {
 			guard.X = guardIdx
 			guard.Y = y
-			grid = append(grid, []rune(strings.Replace(line, "^", ".", -1)))
 		}
+
+		grid = append(grid, []rune(line))
 
 	}
 
--- day6/day6_util.go
+++ day6/day6_util.go
@@ -1,1 +1,57 @@
 package day6
+
+type Vector struct {
+	X      int
+	Y      int
+	Facing rune
+}
+
+type VectorPath struct {
+	path []Vector
+}
+
+func New() *VectorPath {
+	return &VectorPath{
+		path: make([]Vector, 0), // Initializes an empty slice of pointers to Vector
+	}
+}
+
+func (vp *VectorPath) Length() int {
+	return len(vp.path)
+}
+
+func (vp *VectorPath) Append(v Vector) {
+	vp.path = append(vp.path, v)
+}
+
+func (vp *VectorPath) GetConsumedCoordinates() []Vector {
+	copyPaths := make([]Vector, len(vp.path))
+
+	copy(copyPaths, vp.path)
+
+	return copyPaths
+}
+
+func (vp *VectorPath) VectorCausesCycle(v Vector) bool {
+
+	for _, pElement := range vp.path {
+
+		if pElement.Facing == v.Facing && pElement.X == v.X && pElement.Y == v.Y {
+			return true
+		}
+
+	}
+	return false
+}
+
+func (vp *VectorPath) LocationTraversed(v Vector) bool {
+
+	for _, pElement := range vp.path {
+
+		if pElement.X == v.X && pElement.Y == v.Y {
+			return true
+		}
+
+	}
+	return false
+}
