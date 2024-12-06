813b4fddf75b638b90cc20911728401fcc4e564e
mahldcat <mahldcat@users.noreply.github.com>
2024-12-06 00:35:29 -0700

    Day 6 part 1 is passing unit tests (#10)

--- day6/day6.go
+++ day6/day6.go
@@ -1,15 +1,120 @@
 package day6
 
+func UpdateGuardFacing(guard *Vector) {
+	//fmt.Printf("Rotation Guard FROM Facing:(%c,%d,%d)\n", guard.Facing, guard.X, guard.Y)
+	switch guard.Facing {
+	case 'N':
+		guard.Facing = 'E'
+	case 'E':
+		guard.Facing = 'S'
+	case 'S':
+		guard.Facing = 'W'
+	case 'W':
+		guard.Facing = 'N'
+	}
+	//fmt.Printf("             TO Facing:(%c,%d,%d)\n", guard.Facing, guard.X, guard.Y)
+	//fmt.Println("******************************************************************")
+}
+
+func MoveGuard(guard *Vector) {
+	//fmt.Printf("Moving Guard FROM Facing:(%c,%d,%d)\n", guard.Facing, guard.X, guard.Y)
+
+	switch guard.Facing {
+	case 'N':
+		guard.Y--
+	case 'E':
+		guard.X++
+	case 'S':
+		guard.Y++
+	case 'W':
+		guard.X--
+	}
+	//fmt.Printf("               TO Facing:(%c,%d,%d)\n", guard.Facing, guard.X, guard.Y)
+}
+
+func IsFacingObstruction(grid [][]rune, guard *Vector) bool {
+	switch guard.Facing {
+	case 'N':
+		if guard.Y == 0 {
+			return false //facing north at the top of the grid
+		}
+		return grid[guard.Y-1][guard.X] == '#'
+
+	case 'E':
+		if guard.X == (len(grid[0]) - 1) {
+			return false //facing east on the right side of the grid
+		}
+		return grid[guard.Y][guard.X+1] == '#'
+
+	case 'S':
+		if guard.Y == (len(grid) - 1) {
+			return false //facing south bottom of grid
+		}
+		return grid[guard.Y+1][guard.X] == '#'
+
+	case 'W':
+		if guard.X == 0 {
+			return false //facing west on the left side of the grid
+		}
+		return grid[guard.Y][guard.X-1] == '#'
+	}
+	//should never reach here!
+	return false
+}
+
+//return is escape,facingChange
+func UpdateGuardStep(grid [][]rune, guard *Vector) (bool, bool) {
+
+	facingChange := IsFacingObstruction(grid, guard)
+	//fmt.Printf("Guard UPDATE Facing:(%c,%d,%d) changeType:%t \n", guard.Facing, guard.X, guard.Y, facingChange)
+
+	if facingChange {
+		UpdateGuardFacing(guard)
+	} else {
+		MoveGuard(guard)
+	}
+
+	return guard.X < 0 || guard.Y < 0 || guard.X == len(grid[0]) || guard.Y == len(grid), facingChange
+}
+
+func alreadyVisited(visitMap map[int]map[int]bool, guard *Vector) bool {
+
+	x := guard.X
+	y := guard.Y
+	innermap, existsy := visitMap[y]
+
+	if !existsy {
+		innermap = make(map[int]bool)
+		visitMap[y] = innermap
+	}
+
+	_, existsx := innermap[x]
+
+	if !existsx {
+		innermap[x] = true
+	}
+
+	return existsx
+}
 
 func SolveDay6Part1(rawData string) int {
-    solnResult:=-1
+	solnResult := 0
 
-    return solnResult
+	grid, guard, _ := day6DataParser(rawData)
+
+	visitedSpaces := make(map[int]map[int]bool)
+
+	for escape, facingChange := false, false; !escape; escape, facingChange = UpdateGuardStep(grid, &guard) {
+		if !facingChange && !alreadyVisited(visitedSpaces, &guard) {
+			solnResult++
+		}
+	}
+
+	return solnResult
 }
 
 func SolveDay6Part2(rawData string) int {
-    solnResult:=-1
+	solnResult := -1
 
-    return solnResult
+	return solnResult
 }
-
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -4,14 +4,40 @@ import (
 	"testing"
 )
 
-var exampleRaw = ""
+var exampleRaw = `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`
 
 func TestParser(t *testing.T) {
+	expectedGuard := Vector{X: 4, Y: 5}
+	expectedGridXSz := 10
+	expectedGridYSz := 10
 
+	grid, guard, err := day6DataParser(exampleRaw)
+	if err != nil {
+		t.Fatalf("Failure in parsing %v", err)
+	}
+
+	if guard.X != expectedGuard.X && guard.Y != expectedGuard.Y {
+		t.Fatalf("unexpected guard location (%d,%d) expected: (%d,%d)", guard.X, guard.Y, expectedGuard.X, expectedGuard.Y)
+	}
+	if len(grid) != expectedGridYSz {
+		t.Fatalf("unexpected row cound in grid %d, expected %d", len(grid), expectedGridYSz)
+	}
+	if len(grid[0]) != expectedGridXSz {
+		t.Fatalf("unexpected row cound in grid %d, expected %d", len(grid[0]), expectedGridXSz)
+	}
 }
 
 func TestDay6Part1(t *testing.T) {
-	expected := -1
+	expected := 41
 	sln := SolveDay6Part1(exampleRaw)
 
 	if sln != expected {
