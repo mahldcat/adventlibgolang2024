d50b68021ea12c70ee3fac5361da8947f5890460
mahldcat <mahldcat@gmail.com>
2024-12-07 14:31:20 -0700

    Day 7.2 solution

--- day7/day7_util.go
+++ day7/day7_util.go
@@ -1,12 +1,37 @@
 package day7
 
-func HasSolution(total int, subtotal int, remainder []int) bool {
+import (
+	"fmt"
+	"strconv"
+)
+
+func concat(a int, b int) int {
+
+	//I could do this as this one liner....but readability suffers
+	//ab,_ := strconv.Atoi( fmt.Sprintf("%s%s",  strconv.Itoa(a),strconv.Itoa(b)))
+	//and then you realize....I don't need to convert int back to string (idjut)
+
+	concatRaw := fmt.Sprintf("%d%d", a, b)
+	parsed, _ := strconv.Atoi(concatRaw)
+
+	return parsed
+}
+
+func HasSolution(total int, subtotal int, remainder []int, includeConCat bool) bool {
 	if len(remainder) == 0 {
 		return subtotal == total
 	}
 
-	if HasSolution(total, subtotal+remainder[0], remainder[1:]) {
+	if HasSolution(total, subtotal+remainder[0], remainder[1:], includeConCat) {
 		return true
 	}
-	return HasSolution(total, subtotal*remainder[0], remainder[1:])
+	if HasSolution(total, subtotal*remainder[0], remainder[1:], includeConCat) {
+		return true
+	}
+
+	if includeConCat {
+		return HasSolution(total, concat(subtotal, remainder[0]), remainder[1:], includeConCat)
+	}
+
+	return false
 }
