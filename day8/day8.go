25b20b8a7ecb471fec13d5ce05deeb904f9844d7
mahldcat <mahldcat@users.noreply.github.com>
2024-12-08 01:23:51 -0700

    solution 8.1 is passing unit tests (#16)

--- day8/day8.go
+++ day8/day8.go
@@ -1,15 +1,47 @@
 package day8
 
+func ContainsPoint(hayStack []Point, needle Point) bool {
+	for _, p := range hayStack {
+		if p.Equals(needle) {
+			return true
+		}
+	}
+	return false
+}
+
+func AppendBoundaryUnique(points []Point, p Point, gSz Point) []Point {
+	if p.InBounds(gSz) && !ContainsPoint(points, p) {
+		points = append(points, p)
+	}
+
+	return points
+}
 
 func SolveDay8Part1(rawData string) int {
-    solnResult:=-1
 
-    return solnResult
+	pointMap, gridSize := day8DataParser(rawData)
+
+	collectedPoints := make([]Point, 0)
+
+	for _, points := range pointMap {
+
+		for i, p1 := range points {
+			for _, p2 := range points[i+1:] {
+
+				p3, p4 := p1.GetExtensionPoints(p2)
+
+				collectedPoints = AppendBoundaryUnique(collectedPoints, p3, gridSize)
+				collectedPoints = AppendBoundaryUnique(collectedPoints, p4, gridSize)
+			}
+		}
+
+	}
+
+	return len(collectedPoints)
 }
 
 func SolveDay8Part2(rawData string) int {
-    solnResult:=-1
+	solnResult := -1
 
-    return solnResult
+	return solnResult
 }
-
--- day8/day8_dataparser.go
+++ day8/day8_dataparser.go
@@ -1,9 +1,49 @@
 package day8
 
 import (
-	"errors"
+	"bufio"
+	"strings"
 )
 
-func day5DataParser(rawData string) (error) {
-    return errors.New("not implemented")
+/*
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+
+ignore the .
+map[rune][]Vector
+
+*/
+
+func day8DataParser(rawData string) (map[rune][]Point, Point) {
+	reader := strings.NewReader(rawData)
+	scanner := bufio.NewScanner(reader)
+
+	pointMap := make(map[rune][]Point)
+
+	var y int
+	var line string
+	for y = 0; scanner.Scan(); y++ {
+		line = scanner.Text()
+		if line == "" { // Skip empty lines
+			continue
+		}
+
+		for x, val := range line {
+			if val != '.' {
+				p := Point{X: x, Y: y}
+				pointMap[val] = append(pointMap[val], p)
+			}
+		}
+	}
+	return pointMap, Point{X: len(line), Y: y}
 }
