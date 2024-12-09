030cd9b65258cd008a509affa182e18342bcc599
mahldcat <mahldcat@users.noreply.github.com>
2024-12-09 00:11:03 -0700

    update for v0.9.1 (#18)

--- day9/day9_test.go
+++ day9/day9_test.go
@@ -4,14 +4,36 @@ import (
 	"testing"
 )
 
-var exampleRaw = ""
+var exampleRaw = "2333133121414131402"
 
-func TestParser(t *testing.T) {
+func TestParsing(t *testing.T) {
+	vals, spaceNeeded := day9DataParser(exampleRaw)
 
+	expectedValCt := 19
+	expected := 42
+
+	if spaceNeeded != expected {
+		t.Fatalf("mismatched space length expected %d, actual:%d", expected, spaceNeeded)
+	}
+	if len(vals) != expectedValCt {
+		t.Fatalf("mismatched val count, expected %d, actual:%d", expectedValCt, len(vals))
+	}
+}
+
+func TestUnCompacted(t *testing.T) {
+	vals, uncompressedSz := day9DataParser(exampleRaw)
+
+	uncompacted := GetUncompacted(vals, uncompressedSz)
+	expected := 42
+
+	if len(uncompacted) != expected {
+		t.Logf("uncompacted: %v", uncompacted)
+		t.Fatalf("mismatched space length expected %d, actual:%d", expected, len(uncompacted))
+	}
 }
 
 func TestDay9Part1(t *testing.T) {
-	expected := -1
+	expected := 1928
 	sln := SolveDay9Part1(exampleRaw)
 
 	if sln != expected {
